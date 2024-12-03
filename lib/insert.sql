3dc233220bdade06a814a8627a1029c682efe2f6
Akanle Jeremy <akanlejeremy@gmail.com>
2024-12-03 14:56:47 +0300

    sql basics

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'brown','calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 10, 'M', 'black', 'rowdy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 7, 'F', 'grey', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 9, 'M', 'brown', 'reserved', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 9, 'F', 'black', 'unknown', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 8, 'M', 'grey', 'rowdy',  1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 11, 'F', 'brown', 'calm',  1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 8, 'M', 'light brown', 'reserved', 0);
